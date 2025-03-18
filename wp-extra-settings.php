d63032fa66e50f60610d62a52c8a2786fccdca6b
itmaroon <itmaroon@icloud.com>
2025-03-18 14:23:35 +0900

    3-18

--- wp-extra-settings.php
+++ wp-extra-settings.php
@@ -13,6 +13,6 @@ Domain Path:  /languages
 
 if (! defined('ABSPATH')) exit;
 
-//require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-//\Itmar\WpSettingClassPackage\ItmarRevisionClass::get_instance();
+\Itmar\WpSettingClassPackage\ItmarRevisionClass::get_instance();
